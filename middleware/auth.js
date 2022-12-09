ad614f110d2d2ea1ef6cf15153c3c89d0f122c10
muksis <60219890+muksis@users.noreply.github.com>
2022-12-09 15:33:40 -0500

    Add unauthenicated error to auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,10 @@
+import { UnAuthenticatedError } from '../errors/index.js';
+
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log(authHeader);
+  if (!authHeader) {
+    throw new UnAuthenticatedError('Authentication Invalid');
+  }
   next();
 };
 
